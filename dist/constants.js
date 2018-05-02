71d2137fadaa04bcd1270e69c94d1a9db57174e3
yassine <yechabbi@gmail.com>
2018-05-02 13:55:21 +0100

    dist fix

--- dist/constants.js
+++ dist/constants.js
@@ -1,19 +1,10 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.MAGIC_VALUE = exports.MAGIC_KEY = exports.SHA = undefined;
-
-var _semver = require('semver');
-
-var _package = require('../package.json');
-
-var _crypto = require('crypto');
+import { major } from 'semver';
+import { name, version } from '../package.json';
+import { createHash } from 'crypto';
 
 // function to use for creating hashes
-var SHA = exports.SHA = 'sha1';
+export const SHA = 'sha1';
 // name of coverage data magic key
-var MAGIC_KEY = exports.MAGIC_KEY = '_coverageSchema';
+export const MAGIC_KEY = '_coverageSchema';
 // name of coverage data magic value
-var MAGIC_VALUE = exports.MAGIC_VALUE = (0, _crypto.createHash)(SHA).update(_package.name + '@' + (0, _semver.major)(_package.version)).digest('hex');
+export const MAGIC_VALUE = createHash(SHA).update(name + '@' + major(version)).digest('hex');
